05919a9a2dbcb14c1af6563ad83fe1e66c2320b9
Shawn McCool <shawn@heybigname.com>
2014-05-07 17:12:43 +0200

    i had a realization about what's valid inside markdown symbols, i'm going to start changing things, first with the strong tag

--- src/Tags/StrongTag.php
+++ src/Tags/StrongTag.php
@@ -6,6 +6,6 @@ class StrongTag implements Tag
 {
     public function render(Node $node, $text)
     {
-        return '**' . $text . '**';
+        return preg_replace('/^(\s*)((\w|\s\w)*)(\s*)$/', '$1**$2**$4', $text);
     }
 }
